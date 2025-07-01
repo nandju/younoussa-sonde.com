576af1549663483018cced329a1a385deb33270b
nandju <nlbmd17@gmail.com>
2025-07-01 14:38:56 +0000

    comment ça marche fini

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/header"
 
 import Footer from "@/components/footer"
-import AboutSection from "@/components/about-section"
+import AboutSection from "@/components/about/about-section"
+import HowItWorksSection from "@/components/how-it-works/how-it-works-section"
 
 export default function Home() {
   return (
     <div>
       <Header />
       <AboutSection />
+      <HowItWorksSection />
     </div>
   )
 }
