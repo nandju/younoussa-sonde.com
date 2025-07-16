ba0e4f270f2afc6ce1a07653c9d609015dbe023b
nandju <nlbmd17@gmail.com>
2025-07-16 02:46:24 +0000

    initial work

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,13 @@
 import Header from "@/components/header"
 
 import Footer from "@/components/footer"
-import AboutSection from "@/components/about/about-section"
-import HowItWorksSection from "@/components/how-it-works/how-it-works-section"
-import ExplorePropertiesSection from "@/components/explore-properties/explore-properties-section"
-import TopPicksSection from "@/components/top-picks/top-picks-section"
-import FAQSection from "@/components/faq/faq-section"
-import ContactSection from "@/components/contact/contact-section"
 import FooterSection from "@/components/footer"
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <AboutSection />
-      <HowItWorksSection />
-      <ExplorePropertiesSection />
-      <TopPicksSection />
-      <FAQSection />
-      <ContactSection />
+
       <FooterSection />
     </div>
   )
